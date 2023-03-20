ee2684932ff3f85aa9e7fb930dd43944f5bc9520
cioby09 <claudiociobanu09@gmail.com>
2023-03-20 22:02:03 +0100

    Feat: "generate random number" function

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
 // Consegna
-// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+// Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco (attenzione: non bisogna copiare tutta la cartella dell'esercizio ma solo l'index.html, e le cartelle js/ css/ con i relativi script e fogli di stile, per evitare problemi con l'inizializzazione di git).
 
-// Ogni cella ha un numero progressivo, da 1 a 100.
+// Generare una griglia di gioco quadrata in cui ogni cella contiene un numero compreso tra 1 e 100.
 
-// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
 
-// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+// I numeri nella lista delle bombe non possono essere duplicati.
+// [23, 65, 1, 4,78,15,....];
+
+// In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+
+// La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
+
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
 
 
 // Dichiaro il numero di quadrati
@@ -38,4 +45,9 @@ function generateGridItem(text) {
 // Gestisco il click degli Item
 function handleItemClick() {
     this.classList.add("skyblue")
+}
+
+// Genera un numero casuale
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
 }
